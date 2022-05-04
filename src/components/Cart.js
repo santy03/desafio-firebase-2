b5f7354b5a6d0b6e832965c23e972bf726b7176c
santy03 <95034966+santy03@users.noreply.github.com>
2022-05-03 22:19:15 -0300

    Add files via upload

---
+++ src/components/Cart.js
@@ -0,0 +1,96 @@
+import { Link } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import { CartContext } from './CartContext';
+import { serverTimestamp } from "firebase/firestore;"
+
+const Cart = () => {
+    const test = useContext(CartContext);
+
+    const checkout = () => {
+        let order = {
+          buyer: {
+            name: "Santiago Cachola",
+            email: "santiagocachola@2003.com",
+            phone: "1234567"
+          },
+          date: serverTimestamp(),
+          items: test.cartList.map,(item =>({
+            id: 
+            title:
+            price:
+            qty:
+          })) 
+          total: test.calcTotal()
+        }
+        console.log(order);
+    }
+
+    return (
+        <WrapperCart>
+            <TitleCart>YOUR CART</TitleCart>
+            <Top>
+                <Link to='/'><TopButton>CONTINUE SHOPPING</TopButton></Link>
+                {
+                    (test.cartList.length > 0)
+                    ? <TopButton type="filled" onClick={test.removeList}>DELETE ALL PRODUCTS</TopButton>
+                    : <TopText>Your cart is empty</TopText>
+                }
+            </Top>
+            <ContentCart>
+            <Bottom>
+                <Info>
+                    {
+                        test.cartList.length > 0 &&
+                            test.cartList.map(item => 
+                            <Product key={item.idItem}>
+                            <ProductDetail>
+                                <ImageCart src={item.imgItem} />
+                                <Details>
+                                <span>
+                                    <b>Product:</b> {item.nameItem}
+                                </span>
+                                <TopButton type="filled" onClick={() => test.deleteItem(item.idItem)}>DELETE</TopButton>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>
+                                <ProductAmountContainer>
+                                <ProductAmount>{item.qtyItem} item(s)</ProductAmount>
+                                /
+                                <ProductAmount>$ {item.costItem} each</ProductAmount>
+                                </ProductAmountContainer>
+                                <ProductPrice>$ {test.calcTotalPerItem(item.idItem)} </ProductPrice>
+                            </PriceDetail>
+                            </Product>
+                            )
+                    }
+                </Info>
+                {
+                    test.cartList.length > 0 &&
+                        <Summary>
+                            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                            <SummaryItem>
+                                <SummaryItemText>Subtotal</SummaryItemText>
+                                <SummaryItemPrice><FormatNumber number={test.calcSubTotal()} /></SummaryItemPrice>
+                            </SummaryItem>
+                            <SummaryItem>
+                                <SummaryItemText>Taxes</SummaryItemText>
+                                <SummaryItemPrice><FormatNumber number={test.calcTaxes()} /></SummaryItemPrice>
+                            </SummaryItem>
+                            <SummaryItem>
+                                <SummaryItemText>Taxes Discount</SummaryItemText>
+                                <SummaryItemPrice><FormatNumber number={-test.calcTaxes()} /></SummaryItemPrice>
+                            </SummaryItem>
+                            <SummaryItem type="total">
+                                <SummaryItemText>Total</SummaryItemText>
+                                <SummaryItemPrice><FormatNumber number={test.calcTotal()} /></SummaryItemPrice>
+                            </SummaryItem>
+                            <Button onClick= {checkout}>CHECKOUT NOW</Button>
+                        </Summary>
+                }
+            </Bottom>
+            </ContentCart>
+        </WrapperCart>
+    );
+}
+
+export default Cart;
